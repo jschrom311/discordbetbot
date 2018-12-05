8ee5629bfddcf38f08a137d70d80f2fd3eedc583
jschrom311 <jschrom311@yahoo.com>
2018-12-04 18:46:45 -0600

    onready

--- discordbot.py
+++ discordbot.py
@@ -6,3 +6,13 @@ import chalk
 
 bot = commands.Bot(command_prefix='#')
 
+#on ready event
+
+@bot.event
+async def on_ready():
+    print ("Hands up!  Ready to bet!")
+    print (bot.user.name)
+    print (bot.user.id)
+
+#Make the bot run - insert discord authentication token
+#bot.run("token")
